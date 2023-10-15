6529086bc486e93a7faf29c0045287dacf8693c9
MuhdHanish <muhammedhanish11@gmail.com>
2023-10-15 19:43:29 +0530

    open-closed-principle

--- open-close-principle/exam.js
+++ open-close-principle/exam.js
@@ -1,42 +1,70 @@
+class BooleanQuestion{
+    constructor(question) {
+        this.question = question;
+    }
+    printAnswerCase() {
+        console.log("1. true");
+        console.log("2. false");
+    }
+}
+
+class ChoiceQuestion{
+    constructor(question) {
+        this.question = question;
+    }
+    printAnswerCase() {
+        this.question.choices.forEach((choice, index) => {
+            console.log(`${index + 1} - ${choice}`);
+        })
+    }
+}
+
+class TextQuestion{
+    constructor(question) {
+        this.question = question;
+    }
+    printAnswerCase() {
+        console.log("Ans: __________");
+    }
+}
+
+class RangeQuestion{
+    constructor(question) {
+        this.question = question;
+    }
+    printAnswerCase() {
+        console.log("Min: 1");
+        console.log("Max: 5");
+    }
+}
+
 function questionPaper(questions) {
     questions.forEach(element => {
-        console.log(element.question);
-        switch (element.type) {
-            case "boolean":
-                console.log("1. true");
-                console.log("2. false");
-                break;
-            case "choice":
-                element.choices.forEach((choice,index) => {
-                    console.log(`${index + 1} - ${choice}`);
-                })
-                break;
-            case "text":
-                console.log("Answer: _____________");
-                break;
-            default:
-                break;
-        };
-        console.log("");
+        console.log(element.question.question);
+        element.printAnswerCase();
     });
 };
 
 const questions = [
-    {
+    new BooleanQuestion({
         type: "boolean",
         question: "JavaScript is a front-end language ?"
-    },
-    {
+    }),
+    new ChoiceQuestion({
         type: "choice",
         question: "Which is the runtime enviornment of JavaScript ?",
         choices: ["React js", "Node js", "Angular Js"]
-    },
-    {
+    }),
+    new TextQuestion({
         type: "text",
         question: "Who is the father of JavaScript ?",
-    }
+    }),
+    new RangeQuestion({
+        type: "range",
+        question: "On a scale of 1 to 5, how would you rate your experience with asynchronous JavaScript and Promises?",
+    })
 ];
 
 questionPaper(questions);
 
-// Here if i need to add a new question type that leed to change the implemention of the actual code
+// In this case the print open for exentedable but closed for modification
